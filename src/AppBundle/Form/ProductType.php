8735d4ea866cedbc70b667229f994aa6b146b3c4
Thiago F <thiagofigueiredo777@gmail.com>
2016-05-20 15:03:40 +0200

    Password field and encrypt. Run update on database scheme

--- src/AppBundle/Form/ProductType.php
+++ src/AppBundle/Form/ProductType.php
@@ -5,6 +5,7 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 
 class ProductType extends AbstractType
 {
@@ -19,10 +20,10 @@ class ProductType extends AbstractType
             ->add('description')
             ->add('available')
             ->add('user')
-            ->add('categories')
+            ->add('categories', CheckboxType::class)
         ;
     }
-    
+
     /**
      * @param OptionsResolver $resolver
      */
